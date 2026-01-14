ef948a33b7cbaf1eaecd3cba45fe6b1651304578
yousef <yousef.ahmed7@gmail.com>
2026-01-14 23:58:26 +0200

    db schema added

---
+++ backend/src/db/index.ts
@@ -0,0 +1,30 @@
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+import * as schema from "./schema";
+import { ENV } from "../config/env";
+
+if (!ENV.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not set in environment variables");
+}
+
+// initialize PostgreSQL connection pool
+const pool = new Pool({ connectionString: ENV.DATABASE_URL });
+
+// log when first connection is made
+pool.on("connect", () => {
+  console.log("Database connected successfully ✅");
+});
+
+// log when an error occurs
+pool.on("error", (err) => {
+  console.error("💥 Database connection error:", err);
+});
+
+export const db = drizzle({ client: pool, schema });
+
+// 👀 What is a Connection Pool?
+// A connection pool is a cache of database connections that are kept open and reused.
+
+// 🤷‍♂️ Why use it?
+// 🔴 Opening/closing connections is slow. Instead of creating a new connection for each request, we reuse existing ones.
+// 🔴 Databases limit concurrent connections. A pool manages a fixed number of connections and shares them across requests.
